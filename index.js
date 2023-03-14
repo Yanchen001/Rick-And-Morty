590caea2a5017e0eb4c312d9149dba0287c59f1f
Yan Chen <ychen90@outlook.com>
2023-03-14 11:27:07 +0100

    add fetch function

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +15,13 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+
+async function fetchCharacters(){
+  const response = await fetch("https://rickandmortyapi.com/api/character/")
+  const data = await response.json();
+  console.log(data)
+}
+
+fetchCharacters()
+
